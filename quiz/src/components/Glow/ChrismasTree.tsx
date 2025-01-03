79ee5e73b339949ebbdd4d7bd3c889e9166d32cc
future-html <witsanuporn19646@gmail.com>
2025-01-03 09:15:41 +0700

    setup quiz-app

---
+++ quiz/src/components/Glow/ChrismasTree.tsx
@@ -0,0 +1,65 @@
+const ChrismasTree = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="318"
+			height="320"
+			viewBox="0 0 318 320"
+			fill="none"
+		>
+			<g filter="url(#filter0_f_4_2704)">
+				<path
+					d="M172.25 310.333V265.833H225.25V310.333H172.25ZM92.75 310.333V251H0L51.0125 162H26.5L119.25 13.6667L212 162H187.487L238.831 251H145.75V310.333H92.75ZM255.062 251L212 176.833H237.506L167.281 64.4708L198.75 13.6667L291.5 162H266.987L318 251H255.062Z"
+					fill="#06DCEB"
+				/>
+				<path
+					d="M172.25 310.333V265.833H225.25V310.333H172.25ZM92.75 310.333V251H0L51.0125 162H26.5L119.25 13.6667L212 162H187.487L238.831 251H145.75V310.333H92.75ZM255.062 251L212 176.833H237.506L167.281 64.4708L198.75 13.6667L291.5 162H266.987L318 251H255.062Z"
+					stroke="url(#paint0_linear_4_2704)"
+					stroke-width="10"
+				/>
+			</g>
+			<defs>
+				<filter
+					id="filter0_f_4_2704"
+					x="-12.629"
+					y="0.20372"
+					width="343.258"
+					height="319.13"
+					filterUnits="userSpaceOnUse"
+					color-interpolation-filters="sRGB"
+				>
+					<feFlood
+						flood-opacity="0"
+						result="BackgroundImageFix"
+					/>
+					<feBlend
+						mode="normal"
+						in="SourceGraphic"
+						in2="BackgroundImageFix"
+						result="shape"
+					/>
+					<feGaussianBlur
+						stdDeviation="2"
+						result="effect1_foregroundBlur_4_2704"
+					/>
+				</filter>
+				<linearGradient
+					id="paint0_linear_4_2704"
+					x1="159"
+					y1="13.6667"
+					x2="159"
+					y2="310.333"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop />
+					<stop
+						offset="1"
+						stop-color="#037C85"
+					/>
+				</linearGradient>
+			</defs>
+		</svg>
+	);
+};
+
+export default ChrismasTree;
---
+++ quiz/src/components/Glow/Triangle.tsx
@@ -0,0 +1,51 @@
+const Triangle = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="720"
+			height="830"
+			viewBox="0 0 720 830"
+			fill="none"
+		>
+			<g filter="url(#filter0_f_4_2731)">
+				<path
+					d="M125 705.5L439 129L616.5 330L125 705.5Z"
+					fill="#06DCEB"
+				/>
+				<path
+					d="M125 705.5L439 129L616.5 330L125 705.5Z"
+					stroke="#06DCEB"
+					stroke-width="10"
+				/>
+			</g>
+			<defs>
+				<filter
+					id="filter0_f_4_2731"
+					x="0.609066"
+					y="0.340332"
+					width="743.192"
+					height="829.133"
+					filterUnits="userSpaceOnUse"
+					color-interpolation-filters="sRGB"
+				>
+					<feFlood
+						flood-opacity="0"
+						result="BackgroundImageFix"
+					/>
+					<feBlend
+						mode="normal"
+						in="SourceGraphic"
+						in2="BackgroundImageFix"
+						result="shape"
+					/>
+					<feGaussianBlur
+						stdDeviation="60"
+						result="effect1_foregroundBlur_4_2731"
+					/>
+				</filter>
+			</defs>
+		</svg>
+	);
+};
+
+export default Triangle;
